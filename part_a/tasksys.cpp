6c58552c039078994e3fd97947e022772fc7fdc2
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-12-16 13:29:13 +0800

    multi-threads tasksys: spinning woker threads

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -122,6 +122,7 @@ void TaskSystemParallelThreadPoolSpinning::func() {
         if (aJob.id == -1) continue;
         aJob.runnable->runTask(aJob.id, aJob.num_total_tasks);
         counterLock.lock();
+        printf("counter %d \n", *(aJob.counter));
         *(aJob.counter) -= 1;
         printf("counter %d \n", *(aJob.counter));
         counterLock.unlock();
