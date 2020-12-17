5741c85fb743128973e77218afd53109c5bc2281
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-12-17 10:24:28 +0800

    test run

--- part_b/tasksys.h
+++ part_b/tasksys.h
@@ -53,6 +53,10 @@ class TaskSystemParallelThreadPoolSpinning: public ITaskSystem {
         void sync();
 };
 
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
 /*
  * TaskSystemParallelThreadPoolSleeping: This class is the student's
  * optimized implementation of a parallel task execution engine that uses
@@ -68,6 +72,46 @@ class TaskSystemParallelThreadPoolSleeping: public ITaskSystem {
         TaskID runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                 const std::vector<TaskID>& deps);
         void sync();
+    private:
+        std::vector<std::thread> threads;
+        struct Tuple {
+            IRunnable* runnable;
+            int id;
+            int num_total_tasks;
+            std::atomic<int>* counter;
+            std::condition_variable* counterCond;
+            Tuple() {}
+            Tuple(IRunnable* ir, int i, int n, 
+                std::atomic<int>* c, std::condition_variable* cCnd) {
+                runnable = ir;
+                id = i;
+                num_total_tasks = n;
+                counter = c;
+                counterCond = cCnd;
+            }
+        };
+        struct WorkQueue {
+            std::mutex WQmutex;
+            std::condition_variable WQcond;
+            std::queue<Tuple> workQueue;
+            void push(const Tuple& t) {
+                const std::lock_guard<std::mutex> lock(WQmutex);
+                workQueue.push(Tuple(t.runnable, t.id, 
+                    t.num_total_tasks, t.counter, t.counterCond));
+                WQcond.notify_one();
+            }
+            Tuple pop() {
+                std::unique_lock<std::mutex> lock(WQmutex);
+                while (workQueue.empty()) {
+                    WQcond.wait(lock);
+                }
+                Tuple t = workQueue.front();
+                workQueue.pop();
+                return t;
+            }
+        };
+        WorkQueue workQueue;
+        void func();
 };
 
 #endif
