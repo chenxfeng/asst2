1f21edb3bb72d28b063dd7518f17f83185335931
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-12-19 20:18:20 +0800

    run tasks Async With Deps

--- part_b/tasksys.cpp
+++ part_b/tasksys.cpp
@@ -140,7 +140,7 @@ void TaskSystemParallelThreadPoolSleeping::func() {
             aJob.counterCond->notify_one();
             ///if run async With dependency
             if (taskQueue.size()) {
-                printf("job %d succeed jobs: %d\n", aJob.taskID, taskQueue[aJob.taskID].size());
+                printf("job %d in %d before jobs: %d\n", aJob.taskID, taskQueue.size(), taskQueue[aJob.taskID].size());
                 assert(aJob.taskID < taskQueue.size());
                 ///start the succeed task
                 for (int i = 0; i < taskQueue[aJob.taskID].size(); ++i) {
@@ -168,7 +168,7 @@ void TaskSystemParallelThreadPoolSleeping::func() {
                             workQueue.push(Tuple(tid, handle.first, j, handle.second, 
                                 taskWorks.at(tid), &counterCond));
                         }
-                        printf("job %d ready: %d\n", tid);
+                        printf("job %d ready: %d\n", tid, taskWorks.at(tid)->load());
                     }
                 // } catch(std::exception& e) {
                 //     printf("2 exception catched: %s\n", e.what());
